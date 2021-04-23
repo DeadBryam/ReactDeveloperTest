7ed6a4027dc0bf9b5d14eeaccdfffb983f0ae125
DeadBryam <bryamu.u@gmail.com>
2021-04-23 17:29:05 -0600

    feat: random list generation was added

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,49 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import { useEffect, useState } from "react";
+import getRandomUsers from "../utils/fakeGenerator";
 
 export default function Home() {
+  const [randomList, setRandomList] = useState([]);
+  const [amount, setAmount] = useState(0);
+
+  const onClickHandler = () => {
+    amount > 0
+      ? setRandomList(getRandomUsers(amount))
+      : alert("INSERT ANY AMOUNT");
+
+  };
+
+  const onInputHandler = (evt) => {
+    setAmount(evt.target.value);
+  };
+
   return (
-    <div className={styles.container}>
+    <div className="grid justify-items-center">
       <Head>
         <title>Random User Generator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-       
+      <main className="relative my-14 h-screen max-h-full">
+        <p className="font-sans text-7xl font-bold text-white top-0 black-light">
+          RANDOM FAKE USERS GENERATOR
+        </p>
+        <div className="h-40 w-full bg-gray-800">
+          <input type="number" onChange={onInputHandler} />
+          <button onClick={onClickHandler}>BUSCAR</button>
+        </div>
 
       </main>
 
-     
+      <footer className="text-white h-16 w-screen grid place-items-center">
+        <a
+          href="https://github.com/DeadBryam"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Made by&nbsp; <strong>DeadBryam</strong>
+        </a>
+      </footer>
     </div>
   );
 }
---
+++ utils/fakeGenerator.js
@@ -0,0 +1,22 @@
+import faker from "faker";
+
+export default function fakeGenerator(amount) {
+  let list = [];
+
+  for (let index = 0; index < amount; index++) {
+    list.push({
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      addres: faker.address.streetAddress(),
+      username: faker.internet.userName(),
+      password: faker.internet.password(),
+      age: faker.datatype.number({
+        min: 10,
+        max: 50,
+      }),
+      phone: faker.phone.phoneNumber(),
+    });
+  }
+
+  return list;
+}
